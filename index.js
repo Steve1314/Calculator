20ab6fbd9cb15013fcb5fc3356f925ddb983e315
Vivekk <Vivekk@VIVEKK.com>
2023-04-10 16:12:27 +0530

    changes

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
-let string="";
-let buttons=document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-        button.addEventListener('click',(e)=>{
+var screen = document.querySelector('#screen');
+    var btn = document.querySelectorAll('.btn');
 
-                if(e.target.innerHTML=='='){
-                        string=eval(string);
-                        document.querySelector('input').value=string;
-                }
-                else if(e.target.innerHTML=='AC'){
-                        string=""
-                        document.querySelector('input').value=string;
 
-                }
-                
+    for (item of btn) {
+        item.addEventListener('click', (e) => {
+            btntext = e.target.innerText;
 
-                else{
-                console.log(e.target)
-                string=string + e.target.innerHTML;
-                document.querySelector('input').value=string;
-                }
+            if (btntext == '×') {
+                btntext = '*';
+            }
 
-        })
-})
+            if (btntext == '÷') {
+                btntext = '/';
+            }
+            screen.value += btntext;
+        });
+    }
+
+    function sin() {
+        screen.value = Math.sin(screen.value);
+    }
+
+    function cos() {
+        screen.value = Math.cos(screen.value);
+    }
+
+    function tan() {
+        screen.value = Math.tan(screen.value);
+    }
+
+    function pow() {
+        screen.value = Math.pow(screen.value, 2);
+    }
+
+    function sqrt() {
+        screen.value = Math.sqrt(screen.value, 2);
+    }
+
+    function log() {
+        screen.value = Math.log(screen.value);
+    }
+
+    function pi() {
+        screen.value = 3.14159265359;
+    }
+
+    function e() {
+        screen.value = 2.71828182846;
+    }
+
+    function fact() {
+        var i, num, f;
+        f = 1
+        num = screen.value;
+        for (i = 1; i <= num; i++) {
+            f = f * i;
+        }
+
+        i = i - 1;
+
+        screen.value = f;
+    }
+
+    function backspc() {
+        screen.value = screen.value.substr(0, screen.value.length - 1);
+    }
